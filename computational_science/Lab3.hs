ccdeb600eb1fdbd74b1c41aa852b9b7f9d00afe7
Alexey Shmalko <rasen.dubi@gmail.com>
2015-05-06 08:18:02 +0300

    Finished Lab3

--- computational_science/Lab3.hs
+++ computational_science/Lab3.hs
@@ -1,6 +1,91 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Lab3 (main) where
 
 import Utils
 
+import Data.List
+import qualified Data.Vector as V
+import           Data.Vector ((//))
+
+import Debug.Trace
+import qualified Data.String.Here as H
+
 main :: IO ()
-main = putStrLn "Hello!"
+main = do
+    putStrLn "A ="
+    printMatrix a
+    putStrLn ""
+    putStrLn "P ="
+    printMatrix $ danilevsky a
+
+a = mkMatrix
+    [ [6.26 + a, 1.10 - b, 0.97 + g, 1.24 - d]
+    , [1.10 - b, 4.16 - a,     1.30,     0.16]
+    , [0.97 + g, 1.30,     5.44 + a,     2.10]
+    , [1.24 - d, 0.16,         2.10, 6.10 - a]
+    ]
+    where
+        t = 1
+        k = 3 * (2 - 4) + 1
+        a = 0.11 * t
+        b = 0.02 * k
+        d = 0.015 * t
+        g = 0.02 * k
+
+danilevsky :: Matrix Double -> Matrix Double
+danilevsky m = iteration (V.length m - 1) m
+
+-- i = m - i + 1
+iteration :: Int -> Matrix Double -> Matrix Double
+iteration 0 a = a
+iteration i (normalize i -> Just a) = iteration (i-1) $ m' `mul` a `mul` m
+        `ftrace`
+            [H.i|
+Iteration ${i}
+M  =
+${showMatrix m}
+
+M' =
+${showMatrix m'}
+
+a  =
+${showMatrix a}
+            |]
+    where
+        m = identity len // [(i - 1, V.generate len $ \j -> if j /= i - 1
+                then -a!i!j / a!i!(i-1)
+                else 1 / a!i!(i-1)
+            )]
+        m' = identity len // [(i - 1, V.generate len $ \j -> a!i!j)]
+        len = V.length a
+iteration i a = traceShowId $ mergeTopLeft a $ danilevsky $ topLeft i a
+
+ftrace = flip trace
+
+normalize :: Int -> Matrix Double -> Maybe (Matrix Double)
+normalize i a =
+    if a!i!(i-1) /= 0
+        then return a
+        else (\swapIndex -> swapRowCol i swapIndex a) `fmap` getSwapIndex a i
+
+swapRowCol :: Int -> Int -> Matrix a -> Matrix a
+swapRowCol x y = swap x y . fmap (swap x y)
+
+getSwapIndex :: (Eq a, Num a) => Matrix a -> Int -> Maybe Int
+getSwapIndex m i = flip find [0 .. i-1] $ \x -> m!i!x /= 0
+{-# SPECIALIZE getSwapIndex :: Matrix Double -> Int -> Maybe Int #-}
+
+topLeft :: Int -> Matrix a -> Matrix a
+topLeft i = fmap (V.slice 0 i) . V.slice 0 i
+
+mergeTopLeft :: Matrix a -> Matrix a -> Matrix a
+mergeTopLeft x tl =
+    V.generate x_length $ \i ->
+        V.generate x_length $ \j ->
+            if i < tl_length && j < tl_length
+                then tl!i!j
+                else x!i!j
+    where
+        x_length = V.length x
+        tl_length = V.length tl
--- computational_science/Utils.hs
+++ computational_science/Utils.hs
@@ -3,16 +3,22 @@
 module Utils
     ( Matrix
     , Vector
+    , mkMatrix
     , (V.!)
     , (<$>)
     , on
     , swap
+    , fromMatrix
+    , showMatrix
     , printMatrix
     , printRow
     , diff
     , residual
     , mean
     , sigma
+    , identity
+    , mul
+    , for
     ) where
 
 import Prelude hiding (mapM_)
@@ -20,7 +26,7 @@ import Prelude hiding (mapM_)
 import Data.Function (on)
 
 import qualified Data.Vector as V
-import           Data.Vector (Vector, mapM_, forM_)
+import           Data.Vector (Vector, mapM_, forM_, (!))
 
 import Data.List
 
@@ -30,6 +36,12 @@ import Control.Applicative ((<$>))
 
 type Matrix a = Vector (Vector a)
 
+fromMatrix :: Matrix a -> [[a]]
+fromMatrix = fmap V.toList . V.toList
+
+mkMatrix :: [[a]] -> Matrix a
+mkMatrix = V.fromList . fmap V.fromList
+
 swapInList :: Int -> Int -> [Int] -> [Int]
 swapInList x y = fmap $ \case
     e | e == x    -> y
@@ -38,6 +50,9 @@ swapInList x y = fmap $ \case
 
 swap :: Int -> Int -> Vector a -> Vector a
 swap x y v = V.backpermute v . V.fromList $ swapInList x y [0 .. V.length v - 1]
+
+showMatrix :: Show a => Matrix a -> String
+showMatrix = intercalate "\n" . fmap (intercalate "\t" . fmap show) . fromMatrix
 
 printMatrix :: PrintfArg a => Matrix a -> IO ()
 printMatrix = mapM_ printRow
@@ -60,3 +75,24 @@ sigma x y = sqrt . mean . map (^2) $ zipWith (-) (V.toList x) (V.toList y)
 
 diff :: Vector Double -> Vector Double -> Vector Double
 diff = V.zipWith ((abs .) . (-))
+
+identity :: Num a => Int -> Matrix a
+identity n =
+    V.generate n $ \i ->
+        V.generate n $ \j ->
+            toNum $ i == j
+
+toNum :: Num a => Bool -> a
+toNum True  = 1
+toNum False = 0
+
+mul :: Matrix Double -> Matrix Double -> Matrix Double
+mul x y =
+        V.generate len $ \i ->
+            V.generate len $ \j ->
+                sum $ for [0 .. len-1] $ \k ->
+                    x!i!k * y!k!j
+    where len = V.length x
+
+for :: Functor f => f a -> (a -> b) -> f b
+for = flip fmap
